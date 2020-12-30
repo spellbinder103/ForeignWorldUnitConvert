8302118f6b2a5d5ec5490ded4e727e3899a33d7b
Wong Kean Boon <spellbinder103@yahoo.com>
2020-12-30 23:54:20 +0800

    Add files via upload
    add new

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("异世界单位转换软件")>
+<Assembly: AssemblyDescription("用在纳布星修真文明的单位转换软件")>
+<Assembly: AssemblyCompany("被隐藏的实验室")>
+<Assembly: AssemblyProduct("异世界单位转换器")>
+<Assembly: AssemblyCopyright("被隐藏的实验室 © 2020")>
+<Assembly: AssemblyTrademark("HiddenLabs")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9c55cb77-e6b2-4a1a-a031-2ec081d4a398")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.2.0.0")>
+<Assembly: AssemblyFileVersion("1.2.0.0")>
+<Assembly: NeutralResourcesLanguage("zh-CN")>
