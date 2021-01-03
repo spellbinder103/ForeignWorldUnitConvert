5887dc3fcc7c55063d4d8a9e2b286439ec345c81
Wong Kean Boon <spellbinder103@yahoo.com>
2021-01-03 22:37:01 +0800

    Add files via upload

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.异世界单位转换器.MainForm
+            Me.MainForm = Global.ForeignWorldUnitConvert.MainForm
         End Sub
     End Class
 End Namespace
